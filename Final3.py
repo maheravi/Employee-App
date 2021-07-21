1a00d5e0ded4d36b61eeebb47967178ed08f1ac7
maheravi <56476428+maheravi@users.noreply.github.com>
2021-07-21 15:56:06 +0430

    Update Final3.py

--- Final3.py
+++ Final3.py
@@ -13,7 +13,7 @@ import numpy as np
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 from database import Database
-from datetime import datetime
+from filter import Filter
 
 # Convert an opencv image to QPixmap
 def convertCvImage2QtImage(cv_img):
@@ -23,60 +23,6 @@ def convertCvImage2QtImage(cv_img):
     qImg = QImage(cv_img.data, width, height, bytesPerLine, QImage.Format_RGB888)
 
     return QPixmap.fromImage(qImg)
-
-
-def apply_invert(frame):
-    return cv2.bitwise_not(frame)
-
-
-def apply_sepia(frame, intensity=0.5):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    frame_h, frame_w, frame_d = frame.shape
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-    return frame
-
-
-def gray_scale(frame, intensity=0.5):
-    frame = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
-    return frame
-
-
-def alpha_blend(frame_1, frame_2, mask):
-    alpha = mask/255.0
-    blended = cv2.convertScaleAbs(frame_1*(1-alpha) + frame_2*alpha)
-    return blended
-
-
-def verfy_alpha_channel(frame):
-    try:
-        frame.shape[3]
-    except IndexError:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    return frame
-
-
-def apply_stylize(frame):
-    water_color = cv2.stylization(frame, sigma_s=60, sigma_r=0.6)
-    return water_color
-
-
-def apply_portrait_mode(frame):
-    frame = verfy_alpha_channel(frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
-    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
-    blured = cv2.GaussianBlur(frame, (21, 21), 11)
-    blended = alpha_blend(frame, blured, mask)
-    frame = cv2.cvtColor(blended, cv2.COLOR_BGRA2BGR)
-    return frame
-
-
-def pencil_sketch(frame):
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    grayImageInv = 255 - gray_image
-    grayImageInv = cv2.GaussianBlur(grayImageInv, (21, 21), 0)
-    output = cv2.divide(gray_image, 255-grayImageInv, scale=256.0)
-    return output
 
 
 class FirstForm(QMainWindow):
@@ -216,52 +162,52 @@ class SecondForm(QWidget, QThread):
             #     self.ui2.lbl1.clicked.connect(self.add)
             #     # break
 
-            invert = apply_invert(image_frame)
+            invert = Filter.apply_invert(image_frame)
             self.ui2.lbl2.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(invert)
             self.ui2.lbl2.setIcon(QIcon(my_pixmap))
             self.ui2.lbl2.clicked.connect(self.add)
 
-            portrait = apply_portrait_mode(image_frame)
+            portrait = Filter.apply_portrait_mode(image_frame)
             self.ui2.lbl3.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(portrait)
             self.ui2.lbl3.setIcon(QIcon(my_pixmap))
             self.ui2.lbl3.clicked.connect(self.add)
 
-            pencil = pencil_sketch(frame)
+            pencil = Filter.pencil_sketch(frame)
             self.ui2.lbl4.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(pencil)
             self.ui2.lbl4.setIcon(QIcon(my_pixmap))
             self.ui2.lbl4.clicked.connect(self.add)
 
-            stylize = apply_stylize(image_frame)
+            stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl5.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl5.setIcon(QIcon(my_pixmap))
             self.ui2.lbl5.clicked.connect(self.add)
 
-            gray = gray_scale(image_frame)
+            gray = Filter.gray_scale(image_frame)
             self.ui2.lbl6.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(gray)
             self.ui2.lbl6.setIcon(QIcon(my_pixmap))
             self.ui2.lbl6.clicked.connect(self.add)
 
 
-            stylize = apply_stylize(image_frame)
+            stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl7.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl7.setIcon(QIcon(my_pixmap))
             self.ui2.lbl7.clicked.connect(self.add)
 
 
-            stylize = apply_stylize(image_frame)
+            stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl8.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl8.setIcon(QIcon(my_pixmap))
             self.ui2.lbl8.clicked.connect(self.add)
 
 
-            stylize = apply_stylize(image_frame)
+            stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl9.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl9.setIcon(QIcon(my_pixmap))

089de674b9bf4f337a8838d51de57395983e22c4
maheravi <56476428+maheravi@users.noreply.github.com>
2021-07-21 23:31:24 +0430

    Update and rename Final3.py to Final_D.py

--- Final_D.py
+++ Final_D.py
@@ -132,90 +132,77 @@ class SecondForm(QWidget, QThread):
 
             validation, frame = my_video.read()
             width, height, channel = frame.shape
-            image = np.zeros((width, height, channel), dtype='uint8')
 
             if validation is not True:
                 break
 
-            face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_detector.detectMultiScale(frame_gray, 1.3)
-
-            for i, face in enumerate(faces):
-                x, y, w, h = face
-
-                self.frame_face = frame[y:y + h, x:x + w]
-
-                # if self.ui2.takepicture.clicked is True:
-                #     self.ui2.takepicture.clicked.connect(self.takepicture)
-                #     break
-
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 8)
-
             image_frame = cv2.resize(frame, (height // 3, width // 3))
+
             self.ui2.lbl1.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(image_frame)
             self.ui2.lbl1.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl1.clicked.connect(self.add)
-            # if self.ui2.lbl1.pressed():
-            #     print("yes")
-            #     self.ui2.lbl1.clicked.connect(self.add)
-            #     # break
+            self.ui2.lbl1.clicked.connect(self.picture(image_frame, image_frame))
 
             invert = Filter.apply_invert(image_frame)
             self.ui2.lbl2.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(invert)
             self.ui2.lbl2.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl2.clicked.connect(self.add)
+            self.ui2.lbl2.clicked.connect(self.picture(image_frame, invert))
 
             portrait = Filter.apply_portrait_mode(image_frame)
             self.ui2.lbl3.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(portrait)
             self.ui2.lbl3.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl3.clicked.connect(self.add)
+            self.ui2.lbl3.clicked.connect(self.picture(image_frame, portrait))
 
-            pencil = Filter.pencil_sketch(frame)
+            pencil = Filter.pencil_sketch(image_frame)
             self.ui2.lbl4.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(pencil)
             self.ui2.lbl4.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl4.clicked.connect(self.add)
+            self.ui2.lbl4.clicked.connect(self.picture(image_frame, pencil))
 
-            stylize = Filter.apply_stylize(image_frame)
+            sepia = Filter.sepia(image_frame)
             self.ui2.lbl5.setIconSize(QSize(100, 100))
-            my_pixmap = convertCvImage2QtImage(stylize)
+            my_pixmap = convertCvImage2QtImage(sepia)
             self.ui2.lbl5.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl5.clicked.connect(self.add)
+            self.ui2.lbl5.clicked.connect(self.picture(image_frame, sepia))
 
             gray = Filter.gray_scale(image_frame)
             self.ui2.lbl6.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(gray)
             self.ui2.lbl6.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl6.clicked.connect(self.add)
-
+            self.ui2.lbl6.clicked.connect(self.picture(image_frame, gray))
 
             stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl7.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl7.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl7.clicked.connect(self.add)
-
+            self.ui2.lbl7.clicked.connect(self.picture(image_frame, stylize))
 
             stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl8.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl8.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl8.clicked.connect(self.add)
-
+            self.ui2.lbl8.clicked.connect(self.picture(image_frame, stylize))
 
             stylize = Filter.apply_stylize(image_frame)
             self.ui2.lbl9.setIconSize(QSize(100, 100))
             my_pixmap = convertCvImage2QtImage(stylize)
             self.ui2.lbl9.setIcon(QIcon(my_pixmap))
-            self.ui2.lbl9.clicked.connect(self.add)
-
+            self.ui2.lbl9.clicked.connect(self.picture(image_frame, stylize))
 
             cv2.waitKey(1)
-            #self.showOutput(image)
+
+    def picture(self, image, frame):
+
+        face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(frame_gray, 1.3)
+
+        for i, face in enumerate(faces):
+            x, y, w, h = face
+
+            self.frame_face = frame[y:y + h, x:x + w]
 
     def takepicture(self):
 
@@ -235,8 +222,10 @@ class ThirdForm(QWidget, QThread):
     def __init__(self, parent=None):
         super(ThirdForm, self).__init__(parent)
         loader = QUiLoader()
-        self.ui3 = loader.load("form3.ui")
+        self.ui3 = loader.load("form5.ui")
         self.ui3.btn_edit.clicked.connect(self.edit)
+        self.ui3.btn_camera.clicked.connect(self.webcam)
+        self.frame_face = None
         self.ui3.show()
         indices = window.ui.tableWidget.selectionModel().selectedRows()
         for index in sorted(indices):
@@ -257,16 +246,96 @@ class ThirdForm(QWidget, QThread):
         lastname = self.ui3.tb_lastname.text()
         nationalcode = self.ui3.tb_nationalcode.text()
         dateofbirth = self.ui3.tb_dateofbirth.text()
-        my_con = connect('ListofEmployee.db')
-        my_cursor = my_con.cursor()
-        my_cursor.execute(
-            f"INSERT INTO List_of_Employee(Name, LastName, NationalCode, DateofBirth) VALUES('{name}', '{lastname}', '{nationalcode}', '{dateofbirth}')")
-        my_con.commit()
-        my_con.close()
+        Database.edit(name, lastname, nationalcode, dateofbirth)
         mytuple = (name, lastname, nationalcode, dateofbirth)
         for col in range(0, 4):
             window.ui.tableWidget.setItem(self.row, col, QTableWidgetItem(str(mytuple[col])))
+        cv2.imwrite(f"{nationalcode}.png", self.frame_face)
+        pic = QPixmap(f"{nationalcode}" + '.png')
+        pic_scaled = pic.scaled(100, 100)
+        self.label = QLabel()
+        self.label.setPixmap(pic_scaled)
+        window.ui.tableWidget.setCellWidget(self.row, 4, self.label)
         self.close()
+
+    def picture(self, image, frame):
+
+        face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(frame_gray, 1.3)
+
+        for i, face in enumerate(faces):
+            x, y, w, h = face
+
+            self.frame_face = frame[y:y + h, x:x + w]
+
+    def webcam(self):
+
+        my_video = cv2.VideoCapture(0)
+        while True:
+
+            validation, frame = my_video.read()
+            width, height, channel = frame.shape
+
+            if validation is not True:
+                break
+
+            image_frame = cv2.resize(frame, (height // 3, width // 3))
+
+            self.ui3.lbl1.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(image_frame)
+            self.ui3.lbl1.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl1.clicked.connect(self.picture(image_frame, image_frame))
+
+            invert = Filter.apply_invert(image_frame)
+            self.ui3.lbl2.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(invert)
+            self.ui3.lbl2.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl2.clicked.connect(self.picture(image_frame, invert))
+
+            portrait = Filter.apply_portrait_mode(image_frame)
+            self.ui3.lbl3.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(portrait)
+            self.ui3.lbl3.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl3.clicked.connect(self.picture(image_frame, portrait))
+
+            pencil = Filter.pencil_sketch(image_frame)
+            self.ui3.lbl4.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(pencil)
+            self.ui3.lbl4.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl4.clicked.connect(self.picture(image_frame, pencil))
+
+            sepia = Filter.sepia(image_frame)
+            self.ui3.lbl5.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(sepia)
+            self.ui3.lbl5.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl5.clicked.connect(self.picture(image_frame, sepia))
+
+            gray = Filter.gray_scale(image_frame)
+            self.ui3.lbl6.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(gray)
+            self.ui3.lbl6.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl6.clicked.connect(self.picture(image_frame, gray))
+
+            stylize = Filter.apply_stylize(image_frame)
+            self.ui3.lbl7.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(stylize)
+            self.ui3.lbl7.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl7.clicked.connect(self.picture(image_frame, stylize))
+
+            stylize = Filter.apply_stylize(image_frame)
+            self.ui3.lbl8.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(stylize)
+            self.ui3.lbl8.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl8.clicked.connect(self.picture(image_frame, stylize))
+
+            stylize = Filter.apply_stylize(image_frame)
+            self.ui3.lbl9.setIconSize(QSize(100, 100))
+            my_pixmap = convertCvImage2QtImage(stylize)
+            self.ui3.lbl9.setIcon(QIcon(my_pixmap))
+            self.ui3.lbl9.clicked.connect(self.picture(image_frame, stylize))
+
+            cv2.waitKey(1)
 
 
 app = QApplication(sys.argv)
